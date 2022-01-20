135e2dee946cdcc5fb8bbf194a37244d04780c2f
kanika637 <kanikagola26@gmail.com>
2022-01-20 17:30:39 +0530

    Time-Submitted:01/05/2022 14:55  Runtime:4ms Memory:11.2MB Language:cpp

---
+++ Q-82_RemoveDuplicatesFromSortedList2.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(!head || !head->next) return head;
+        ListNode*next=head->next;
+        
+        if(next->val==head->val){
+            //we are ignoring the duplicates 
+            while(next && next->val==head->val)
+                next=next->next;
+            //next is the new head
+            return deleteDuplicates(next);
+        }else{
+            //we have to inlcude the head
+            head->next=deleteDuplicates(next);
+            return head;
+        }
+        
+    }
+};
